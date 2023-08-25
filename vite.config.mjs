063b4866b66767291ea7779b451f0907464caf5d
uranus-aqua <50594973+uranus-aqua@users.noreply.github.com>
2023-08-25 10:33:32 +0800

    vite configure

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,3 @@
-import path from 'path';
-import { resolve } from 'path';
-import { defineConfig,  loadEnv  } from 'vite';
-import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // const env = loadEnv(mode, process.cwd(), '');
