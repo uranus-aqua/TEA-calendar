0113c0c567c8253c26f042bc11c100ff6a01f45f
uranus-aqua <50594973+uranus-aqua@users.noreply.github.com>
2023-08-25 13:15:29 +0800

    vite conf.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+import path from 'node:path';
+import { resolve } from 'node:path';
+import { defineConfig,  loadEnv  } from 'vite';
+import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// const env = loadEnv(mode, process.cwd(), '');
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ _command }) => {
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
     // vite config
     define: {
